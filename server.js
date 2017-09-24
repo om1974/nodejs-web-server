6432f5d4d57d9c8b598b50b901fc4bf27ae91210
Oliver Michels <oliver.michels@gmx.net>
2017-09-24 07:56:25 +0200

    Added a Projects page

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.set('view engine', 'hbs');
 app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
-  
+
   console.log(log);
   fs.appendFile('server.log', log + '\n', (err) => {
     if (err) {
@@ -56,6 +56,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
